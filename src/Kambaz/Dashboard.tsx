fcd30ffa1b8654142bbe901dd5ae4c9e3de6219d
PeterLi <peterli@PeterLis-MacBook-Air.local>
2025-02-06 00:33:47 +0800

    a2 CSS and Bootstrap

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,3 +1,7 @@
+import { Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function Dashboard() {
   return (
@@ -5,78 +9,95 @@ export default function Dashboard() {
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
+
+      <div id="wd-dashboard-courses">
+    <Row xs={1} md={5} className="g-4">
+   <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+        <Card>
           <Link to="/Kambaz/Courses/1231/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/cs01.jpeg" width={200} />
-            <div>
-              <h5> CS1231 React JS1 </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer1  </p>
-              <button> Go </button>
-            </div>
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+            <Card.Img variant="top" src="/images/cs01.jpeg" width="100%" height={160}/>
+            <Card.Body>
+              <Card.Title className="wd-dashboard-course-title">CS1231 React JS1</Card.Title>
+              <Card.Text  className="wd-dashboard-course-description">Full Stack software developer</Card.Text>
+              <Button variant="primary">Go</Button>
+            </Card.Body>
           </Link>
-        </div>
-        <div className="wd-dashboard-course"> <Link to="/Kambaz/Courses/1232/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/cs02.jpeg" width={200} />
-            <div>
-              <h5> CS1232 React JS2 </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer2  </p>
-              <button> Go </button>
-            </div>
-          </Link> </div>
-        <div className="wd-dashboard-course"> <Link to="/Kambaz/Courses/1233/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/cs03.jpeg" width={200} />
-            <div>
-              <h5> CS1233 React JS3 </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer3  </p>
-              <button> Go </button>
-            </div>
-          </Link> </div>
-        <div className="wd-dashboard-course"> <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/cs04.jpeg" width={200} />
-            <div>
-              <h5> CS1234 React JS4 </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer4  </p>
-              <button> Go </button>
-            </div>
-          </Link> </div>
-        <div className="wd-dashboard-course"> <Link to="/Kambaz/Courses/1235/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/cs05.jpeg" width={200} />
-            <div>
-              <h5> CS1235 React JS5 </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer5  </p>
-              <button> Go </button>
-            </div>
-          </Link> </div>
-        <div className="wd-dashboard-course"> .<Link to="/Kambaz/Courses/1236/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/cs06.jpeg" width={200} />
-            <div>
-              <h5> CS1236 React JS6 </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer6  </p>
-              <button> Go </button>
-            </div>
-          </Link> </div>
-        <div className="wd-dashboard-course"> <Link to="/Kambaz/Courses/1237/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/cs07.jpeg" width={200} />
-            <div>
-              <h5> CS1237 React JS7 </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer7  </p>
-              <button> Go </button>
-            </div>
-          </Link> </div>
+        </Card>
+      </Col>
+      
+      <Col className="wd-dashboard-course" style={{ width: "300px" }}> <Card>
+          <Link to="/Kambaz/Courses/1232/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+            <Card.Img variant="top" src="/images/cs02.jpeg" width="100%" height={160}/>
+            <Card.Body>
+              <Card.Title className="wd-dashboard-course-title">CS1232 React JS2</Card.Title>
+              <Card.Text  className="wd-dashboard-course-description">Full Stack software developer</Card.Text>
+              <Button variant="primary">Go</Button>
+            </Card.Body>
+          </Link>
+        </Card>
+      </Col>
+
+      <Col className="wd-dashboard-course" style={{ width: "300px" }}> <Card>
+          <Link to="/Kambaz/Courses/1233/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+            <Card.Img variant="top" src="/images/cs03.jpeg" width="100%" height={160}/>
+            <Card.Body>
+              <Card.Title className="wd-dashboard-course-title">CS1233 React JS3</Card.Title>
+              <Card.Text  className="wd-dashboard-course-description">Full Stack software developer</Card.Text>
+              <Button variant="primary">Go</Button>
+            </Card.Body>
+          </Link>
+        </Card> </Col>
+      <Col className="wd-dashboard-course" style={{ width: "300px" }}>  <Card>
+          <Link to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+            <Card.Img variant="top" src="/images/cs04.jpeg" width="100%" height={160}/>
+            <Card.Body>
+              <Card.Title className="wd-dashboard-course-title">CS1234 React JS4</Card.Title>
+              <Card.Text  className="wd-dashboard-course-description">Full Stack software developer</Card.Text>
+              <Button variant="primary">Go</Button>
+            </Card.Body>
+          </Link>
+        </Card> </Col>
+      <Col className="wd-dashboard-course" style={{ width: "300px" }}> <Card>
+          <Link to="/Kambaz/Courses/1235/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+            <Card.Img variant="top" src="/images/cs05.jpeg" width="100%" height={160}/>
+            <Card.Body>
+              <Card.Title className="wd-dashboard-course-title">CS1235 React JS5</Card.Title>
+              <Card.Text  className="wd-dashboard-course-description">Full Stack software developer</Card.Text>
+              <Button variant="primary">Go</Button>
+            </Card.Body>
+          </Link>
+        </Card> </Col>
+      <Col className="wd-dashboard-course" style={{ width: "300px" }}> <Card>
+          <Link to="/Kambaz/Courses/1236/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+            <Card.Img variant="top" src="/images/cs06.jpeg" width="100%" height={160}/>
+            <Card.Body>
+              <Card.Title className="wd-dashboard-course-title">CS1236 React JS6</Card.Title>
+              <Card.Text  className="wd-dashboard-course-description">Full Stack software developer</Card.Text>
+              <Button variant="primary">Go</Button>
+            </Card.Body>
+          </Link>
+        </Card> </Col>
+      <Col className="wd-dashboard-course" style={{ width: "300px" }}>  <Card>
+          <Link to="/Kambaz/Courses/1237/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+            <Card.Img variant="top" src="/images/cs07.jpeg" width="100%" height={160}/>
+            <Card.Body>
+              <Card.Title className="wd-dashboard-course-title">CS1237 React JS7</Card.Title>
+              <Card.Text  className="wd-dashboard-course-description">Full Stack software developer</Card.Text>
+              <Button variant="primary">Go</Button>
+            </Card.Body>
+          </Link>
+        </Card> </Col>
+    </Row>
+</div>
+
+       
       </div>
     </div>
 );}
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -6,25 +6,15 @@ import Courses from "./Courses";
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
-       <h1>Kambaz</h1>
-      <table>
-        <tr>
-          <td valign="top">
             <KambazNavigation />
-          </td>
-          <td valign="top">
-
+            <div className="wd-main-content-offset p-3" style={{ marginLeft: "120px" }}>
       <Routes>
         <Route path="/" element={<Navigate to="Account" />} />
         <Route path="/Account/*" element={<Account />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/Courses/:cid/*" element={<Courses />} />
-        <Route path="/Calendar" element={<h1>Calendar</h1>} />
-        <Route path="/Inbox" element={<h1>Inbox</h1>} />
       </Routes>
-      </td>
-        </tr>
-      </table>
+    </div>
     </div>
   );
 }
