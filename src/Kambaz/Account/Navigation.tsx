178555a552ecbe03ded418bd93e88117a84623d0
Peter Li <peter890176@gmail.com>
2025-03-31 10:13:45 +0800

    a6

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
   
   return (
     <div id="wd-account-navigation">
@@ -12,8 +14,10 @@ export default function AccountNavigation() {
         </>
       )}
       {currentUser && (
-        <Link to={`/Kambaz/Account/Profile`}> Profile </Link>
+        <Link to={`/Kambaz/Account/Profile`} className={`nav-link ${active("Profile")}`}> Profile </Link>
       )}
+      {currentUser && currentUser.role === "ADMIN" && (
+       <Link to={`/Kambaz/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link> )}
     </div>
   );
 }
