0308526f1bc779f3082ed29ef044b3be13ad9362
Peter Li <peter890176@gmail.com>
2025-04-08 08:33:31 +0800

    temp

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@react-icons/all-files', 'react-icons']
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MISSING_EXPORT') return;
+        warn(warning);
+      }
+    }
+  }
 })
